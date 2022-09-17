4127eb3fd71110c02ea680d3b55b2bc7c7b24088
MauLight <maulisseluz@gmail.com>
2022-09-17 02:56:31 -0300

    basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid';
 
 
 function App(props) {
-  console.log(props)
-  const subject = props.subject;
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const addTask = (name) => {
+    const newTask = {id: `todo-${nanoid()}`, name, completed: ''};
+    setTasks([...tasks, newTask]);
+  };
+
+  function toggleTaskCompleted(id) {
+    const updatedTask = tasks.map((task) => {
+      if(id === task.id) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    })
+    setTasks(updatedTask)
+    console.log(tasks)
+  };
+
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
+  const taskList = tasks?.map((task) => (
+    <Todo name={task.name} completed={task.completed} id={task.id} key={task.id} toggleTaskCompleted= {toggleTaskCompleted} deleteTask= {deleteTask} />
+  ));
+  
+  const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const taskLength = `${taskList.length} ${taskNoun} remaining.`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <strong>{subject}!</strong>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask= {addTask}/>
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+
+      <h2 id="list-heading">
+        {taskLength}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  {id: 'todo-1', name: 'eat', completed: true},
+  {id: 'todo-2', name: 'work', completed: false},
+  {id: 'todo-3', name: 'sleep', completed: false},
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App subject = 'Elvira' />
+    <App tasks= {DATA} />
 
 );
